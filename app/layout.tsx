53fb72636b42ac0391c547aa9b35049954b4abbc
Thomas Puntin <thomas.puntin@MacBookPro.homenet.telecomitalia.it>
2026-09-05 17:56:27 +0200

    Add Umami analytics tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -102,7 +103,15 @@ export default function RootLayout({
       lang="it"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+  {children}
+
+  <Script
+    src="https://cloud.umami.is/script.js"
+    data-website-id="f249ca3d-9eb0-441c-a274-b025d5aa161d"
+    strategy="afterInteractive"
+  />
+</body>
     </html>
   );
 }
